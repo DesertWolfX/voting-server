96dc7cac12ca588006b2af3beda45b18f419d65b
Unknown <staton1986@icloud.com>
2018-03-08 21:56:11 -0800

    addMovie passes with Map

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
     
@@ -29,17 +29,49 @@ describe('immutability', () => {
 
         it('is immutable', () => {
             let state = List.of('Black Panther', 'Thor Ragnarock');
-            let nextState = addMovie(state, 'Justice League');
+            let nextState = addMovie(state, 'Guardians of the Galaxy Vol 2');
 
             expect(nextState).to.equal(List.of(
                 'Black Panther',
                 'Thor Ragnarock',
-                'Justice League'
+                'Guardians of the Galaxy Vol 2'
             ));
-            
+
         });
 
     });
+    //Same test as above, but this is a NESTED DATA STRUCTURE 
+    describe('a tree', () => {
+
+        /*function addMovie(currentState, movie) {
+            return currentState.set(
+                'movies',
+                currentState.get('movies').push(movie)
+            }
+        */
+        // change the above 'get' to 'update to make the code more concise
+        function addMovie(currentState, movie) {
+          return currentState.update('movies', movies => movies.push(movie));
+        }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Black Panther', 'Thor Ragnarock')
+            });
+            let nextState = addMovie(state, 'Guardians of the Galaxy Vol 2');
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Black Panther',
+                    'Thor Ragnarock',
+                    'Guardians of the Galaxy Vol 2'
+                )
+
+            }));
+
+        })
+
+    })
 
 });
 
