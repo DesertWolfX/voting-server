e84a7a4a329898a9513b804ee94d3e2443f85a74
Unknown <staton1986@icloud.com>
2018-03-11 16:09:24 -0700

    Voting
    Vote on entry
    vote increments the tally for movie being voted on and key vote/pair
     creates if doesn't already exist

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,8 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries, next} from '../src/core';
+
+//Import application logic functions for testing
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
     describe('setEntries', () => {
@@ -34,6 +36,51 @@ describe('application logic', () => {
                     pair: List.of('Black Panther', 'Thor Ragnarok')
                 }),
                 entries: List.of('Guardians of the Galaxy Vol 2')
+            }));
+        });
+    });
+
+    describe('vote', () => {
+        it('creates a tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Black Panther', 'Thor Ragnarok')
+                }),
+                entries: List()
+            });
+            const nextState = vote(state, 'Black Panther');
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Black Panther', 'Thor Ragnarok'),
+                    tally: Map({
+                        'Black Panther': 1
+                    })
+                }),
+                entries: List()
+            }));
+        });
+
+        it('adds to existing tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Black Panther', 'Thor Ragnarok'),
+                    tally: Map({
+                        'Black Panther': 3,
+                        'Thor Ragnarok': 2
+                    })
+                }),
+                entries: List()
+            });
+            const nextState = vote(state, 'Black Panther');
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Black Panther', 'Thor Ragnarok'),
+                    tally: Map({
+                        'Black Panther': 4,
+                        'Thor Ragnarok': 2
+                    })
+                }),
+                entries: List()
             }));
         });
     });
