ffaa8f0bd6efd366995975268dc49074aa9b5223
Unknown <staton1986@icloud.com>
2018-03-11 15:35:03 -0700

    Starting the vote
    State passes next two voting items into vote/pair
    next gets entries and merges into vote/pair key

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
     describe('setEntries', () => {
@@ -22,5 +21,20 @@ describe('application logic', () => {
                 entries: List.of('Black Panther', 'Thor Ragnarok')
             }));
         }); 
+    });
+
+    describe('next', () => {
+        it('takes the next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('Black Panther', 'Thor Ragnarok', 'Guardians of the Galaxy Vol 2')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Black Panther', 'Thor Ragnarok')
+                }),
+                entries: List.of('Guardians of the Galaxy Vol 2')
+            }));
+        });
     });
 });
